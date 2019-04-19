e57b5be68f080dc470f92a9cad5d9535932bb3ca
xuexiangjys <xuexiangjys@163.com>
2019-04-19 09:53:08 +0800

    增加文件下载的接口测试

--- app/src/main/java/com/xuexiang/protobufdemo/fragment/XHttpApiTestFragment.java
+++ app/src/main/java/com/xuexiang/protobufdemo/fragment/XHttpApiTestFragment.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.text.InputType;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,6 +38,8 @@ import com.xuexiang.xutil.net.NetworkUtils;
 import com.xuexiang.xutil.tip.ToastUtils;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -78,6 +81,10 @@ public class XHttpApiTestFragment extends XPageFragment {
     Button btnUpload1;
     @BindView(R.id.btn_upload2)
     Button btnUpload2;
+    @BindView(R.id.btn_download1)
+    Button btnDownload1;
+    @BindView(R.id.btn_download2)
+    Button btnDownload2;
 
     @Override
     protected TitleBar initTitleBar() {
@@ -118,7 +125,7 @@ public class XHttpApiTestFragment extends XPageFragment {
     }
 
     @SingleClick
-    @OnClick({R.id.btn_login, R.id.btn_get_user, R.id.btn_select_file1, R.id.btn_select_file2, R.id.btn_upload1, R.id.btn_upload2})
+    @OnClick({R.id.btn_login, R.id.btn_get_user, R.id.btn_select_file1, R.id.btn_select_file2, R.id.btn_upload1, R.id.btn_upload2, R.id.btn_download1, R.id.btn_download2})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btn_login:
@@ -138,6 +145,12 @@ public class XHttpApiTestFragment extends XPageFragment {
                 break;
             case R.id.btn_upload2:
                 testUpload2();
+                break;
+            case R.id.btn_download1:
+                testDownload1();
+                break;
+            case R.id.btn_download2:
+                testDownload2();
                 break;
             default:
                 break;
@@ -232,9 +245,12 @@ public class XHttpApiTestFragment extends XPageFragment {
         });
     }
 
+    /**
+     * 采用流式上传
+     */
     private void testUpload1() {
         if (StringUtils.isEmpty(etPath1.getText().toString()) && StringUtils.isEmpty(etPath2.getText().toString())) {
-            ToastUtils.toast("请输入文上传件路径！");
+            ToastUtils.toast("请输入上传文件路径！");
             return;
         }
 
@@ -291,9 +307,12 @@ public class XHttpApiTestFragment extends XPageFragment {
                 .setSize(file.length()).build();
     }
 
+    /**
+     * 采用普通打包上传
+     */
     private void testUpload2() {
         if (StringUtils.isEmpty(etPath1.getText().toString()) && StringUtils.isEmpty(etPath2.getText().toString())) {
-            ToastUtils.toast("请输入文上传件路径！");
+            ToastUtils.toast("请输入上传文件路径！");
             return;
         }
 
@@ -316,9 +335,9 @@ public class XHttpApiTestFragment extends XPageFragment {
         final ManagedChannel channel = gRPCChannelUtils.newChannel(SettingSPUtils.getInstance().getApiURL(), PORT);
         //构建服务api代理
         mStub = APIServiceGrpc.newStub(channel);
-        mStub.uploadFile(builder.build(), new SimpleStreamObserver<XHttpApi.UploadFileReply>() {
+        mStub.uploadFile(builder.build(), new SimpleStreamObserver<XHttpApi.FilePathPackage>() {
             @Override
-            protected void onSuccess(XHttpApi.UploadFileReply value) {
+            protected void onSuccess(XHttpApi.FilePathPackage value) {
                 tvGrpcResponse.setText(String.format("文件上传成功数：%d", value.getPathCount()));
                 btnUpload2.setEnabled(true);
             }
@@ -336,7 +355,153 @@ public class XHttpApiTestFragment extends XPageFragment {
                 gRPCChannelUtils.shutdown(channel);
             }
         });
+    }
 
+    /**
+     * 采用流式下载
+     */
+    private void testDownload1() {
+        if (StringUtils.isEmpty(etPath1.getText().toString()) && StringUtils.isEmpty(etPath2.getText().toString())) {
+            ToastUtils.toast("请输入下载文件路径！");
+            return;
+        }
+
+        btnDownload1.setEnabled(false);
+        final ManagedChannel channel = gRPCChannelUtils.newChannel(SettingSPUtils.getInstance().getApiURL(), PORT);
+        //构建服务api代理
+        mStub = APIServiceGrpc.newStub(channel);
+        StreamObserver<XHttpApi.FilePath> observer = mStub.downloadStream(new StreamObserver<XHttpApi.FileInfo>() {
+            @Override
+            public void onNext(XHttpApi.FileInfo value) {
+                String path = PathUtils.getExtDownloadsPath() + "/" + value.getFileName();
+                boolean result = FileIOUtils.writeFileFromBytesByChannel(path, value.getData().toByteArray());
+                showDownResult(path, result);
+            }
+
+            @MainThread
+            @Override
+            public void onError(Throwable t) {
+                tvGrpcResponse.setText(Log.getStackTraceString(t));
+                btnDownload1.setEnabled(true);
+            }
+
+            @MainThread
+            @Override
+            public void onCompleted() {
+                gRPCChannelUtils.shutdown(channel);
+                btnDownload1.setEnabled(true);
+            }
+        });
+
+        if (!StringUtils.isEmpty(etPath1.getText().toString())) {
+            XHttpApi.FilePath filePath1 = getFilePathByPath(etPath1.getText().toString());
+            if (filePath1 != null) {
+                observer.onNext(filePath1);
+            }
+        }
+        if (!StringUtils.isEmpty(etPath2.getText().toString())) {
+            XHttpApi.FilePath filePath2 = getFilePathByPath(etPath2.getText().toString());
+            if (filePath2 != null) {
+                observer.onNext(filePath2);
+            }
+        }
+        observer.onCompleted();
+    }
+
+    private XHttpApi.FilePath getFilePathByPath(String path) {
+        File file = FileUtils.getFileByPath(path);
+        if (file == null) {
+            return null;
+        }
+        return XHttpApi.FilePath.newBuilder()
+                .setPath(file.getName())
+                .build();
+    }
+
+    @MainThread
+    private void showDownResult(String path, boolean result) {
+        if (result) {
+            tvGrpcResponse.setText(String.format("文件下载成功：%s", path));
+        } else {
+            ToastUtils.toast("文件下载失败！");
+        }
+    }
+
+    /**
+     * 采用普通打包下载
+     */
+    private void testDownload2() {
+        if (StringUtils.isEmpty(etPath1.getText().toString()) && StringUtils.isEmpty(etPath2.getText().toString())) {
+            ToastUtils.toast("请输入下载文件路径！");
+            return;
+        }
+
+        btnDownload2.setEnabled(false);
+
+        XHttpApi.FilePathPackage.Builder builder = XHttpApi.FilePathPackage.newBuilder();
+        if (!StringUtils.isEmpty(etPath1.getText().toString())) {
+            String fileName1 = getFileNameByPath(etPath1.getText().toString());
+            if (fileName1 != null) {
+                builder.addPath(fileName1);
+            }
+        }
+        if (!StringUtils.isEmpty(etPath2.getText().toString())) {
+            String fileName2 = getFileNameByPath(etPath2.getText().toString());
+            if (fileName2 != null) {
+                builder.addPath(fileName2);
+            }
+        }
+
+        final ManagedChannel channel = gRPCChannelUtils.newChannel(SettingSPUtils.getInstance().getApiURL(), PORT);
+        //构建服务api代理
+        mStub = APIServiceGrpc.newStub(channel);
+        mStub.download(builder.build(), new StreamObserver<XHttpApi.FilePackage>() {
+            @Override
+            public void onNext(XHttpApi.FilePackage value) {
+                List<String> result = new ArrayList<>();
+                String path;
+                for (XHttpApi.FileInfo fileInfo: value.getFileList()) {
+                    path = PathUtils.getExtDownloadsPath() + "/" + fileInfo.getFileName();
+                    if (FileIOUtils.writeFileFromBytesByChannel(path, fileInfo.getData().toByteArray())) {
+                        result.add(path);
+                    }
+                }
+                showDownResult(result);
+            }
+
+            @MainThread
+            @Override
+            public void onError(Throwable t) {
+                tvGrpcResponse.setText(Log.getStackTraceString(t));
+                btnDownload2.setEnabled(true);            }
+
+            @Override
+            public void onCompleted() {
+                gRPCChannelUtils.shutdown(channel);
+            }
+        });
+    }
+
+    @MainThread
+    private void showDownResult(List<String> result) {
+        if (result.size() > 0) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < result.size(); i++) {
+                sb.append("文件下载成功：").append(result.get(i)).append("\r\n");
+            }
+            tvGrpcResponse.setText(sb);
+        } else {
+            ToastUtils.toast("文件下载失败！");
+        }
+        btnDownload2.setEnabled(true);
+    }
+
+    private String getFileNameByPath(String path) {
+        File file = FileUtils.getFileByPath(path);
+        if (file == null) {
+            return null;
+        }
+        return file.getName();
     }
 
 
